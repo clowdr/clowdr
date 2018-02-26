c44dba9e3383dee2fc2ab54418aaa8bca586d9ca
Greg Kiar <gkiar07@gmail.com>
2018-02-26 14:23:18 -0500

    exposed CLI calls through Python API

--- clowdr/__init__.py
+++ clowdr/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from .task import process_task
 from .controller import metadata
+from .driver import dev, deploy, share
 
-__all__ = ['task', 'controller']
+__all__ = ['task', 'controller', 'driver']
 
