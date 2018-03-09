3f59db84259061e76a00fcaf934a09066478e589
gkiar <gkiar07@gmail.com>
2018-03-09 12:42:58 -0500

    updated web service with durations and status

--- clowdr/share/server.py
+++ clowdr/share/server.py
@@ -9,6 +9,7 @@
 
 from flask import Flask, render_template
 import os.path as op
+import datetime
 import tempfile
 import boto3
 import json
@@ -21,14 +22,10 @@ app = Flask(__name__)
 
 @app.route("/clowdr/")
 def index():
-    return render_template('index.html',
-                           data=app.config.get("data"),
-                           bucket=app.config.get("bucket"),
-                           session=app.config.get("offset"),
-                           tasks=app.config.get("tasks"))
+    return render_template('index.html', data=app.config.get("data"))
 
 
-def getJSON(outdir, objlist, buck, cli, **kwargs):
+def parseJSON(outdir, objlist, buck, cli, **kwargs):
     tmplist = []
     for obj in objlist:
         tmpdict = {}
@@ -52,23 +49,36 @@ def getJSON(outdir, objlist, buck, cli, **kwargs):
             tmpdict["name"] = tmpdict["contents"]["name"]
         elif kwargs.get("invocation"):
             tmpdict["name"] = op.basename(key)
+        elif kwargs.get("summary"):
+            tmpdict["id"] = op.splitext(op.basename(outfname))[0].split('-')[1]
         elif kwargs.get("task"):
+            tmpdict["id"] = op.splitext(op.basename(outfname))[0].split('-')[1]
             if kwargs.get("data").get("descriptor"):
                 tmpdict["toolname"] = kwargs["data"]["descriptor"]["name"]
                 tmpdict["toolurl"] = kwargs["data"]["descriptor"]["url"]
+                tmpdict["toolcontents"] = kwargs["data"]["descriptor"]["contents"]
             if kwargs.get("data").get("invocation"):
                 invocs = kwargs["data"]["invocation"]
                 invname = op.basename(tmpdict["contents"]["invocation"])
-                invoc = [inv for inv in invocs if inv["name"] == invname]
-                tmpdict["invocname"] = invoc[0]["name"]
-                tmpdict["invocurl"] = invoc[0]["url"]
+                invoc = [inv for inv in invocs if inv["name"] == invname][0]
+                tmpdict["invocname"] = invoc["name"]
+                tmpdict["invocurl"] = invoc["url"]
+                tmpdict["invocontents"] = invoc["contents"]
+            if kwargs.get("data").get("summary"):
+                summars = kwargs["data"]["summary"]
+                summar = [summ for summ in summars if summ["id"] == tmpdict["id"]]
+                if len(summar) > 0:
+                    tmpdict["exitcode"] = summar[0]["contents"]["exitcode"]
+                    tmpdur = float(summar[0]["contents"]["duration"])
+                    tmpdict["duration"] = str(datetime.timedelta(seconds=tmpdur))
+                    tmpdict["outputs"] = summar[0]["contents"]["outputs"]
 
         tmplist.append(tmpdict)
     return tmplist
 
 
-def main():
-    clowdrloc = sys.argv[1]
+def updateRecord():
+    clowdrloc = app.config.get("clowdrloc")
     tmpdir = tempfile.mkdtemp()
 
     s3 = boto3.resource("s3")
@@ -78,30 +88,27 @@ def main():
     buck = s3.Bucket(bucket)
     objs = buck.objects.filter(Prefix=offset)
     task_objs = [obj for obj in objs if "task-" in obj.key]
+    summary_objs = [obj for obj in objs if "summary-" in obj.key]
     descriptor_obj = [obj for obj in objs if "descriptor" in obj.key]
     invocation_obj = [obj for obj in objs if "invocation" in obj.key]
 
-    descriptor = getJSON(tmpdir, descriptor_obj, buck, cli, descriptor=True)[0]
-    invocation = getJSON(tmpdir, invocation_obj, buck, cli, invocation=True)
-    tasks = getJSON(tmpdir, task_objs, buck, cli, task=True,
-                    data={"descriptor": descriptor, "invocation": invocation})
-    # dbuck, dkey = re.match('^s3:\/\/([\w\-\_]+)/([\w\-\_\/\.]+)',
-    #                        task["contents"]["tool"]).group(1, 2)
-    # task["contents"]["tool"] = cli.generate_presigned_url('get_object',
-    #                                                       Params={'Bucket': dbuck,
-    #                                                               'Key': dkey},
-    #                                                       ExpiresIn=None)
-    # ibuck, ikey = re.match('^s3:\/\/([\w\-\_]+)/([\w\-\_\/\.]+)',
-    #                        task["contents"]["invocation"]).group(1, 2)
-    # task["contents"]["invocation"] = cli.generate_presigned_url('get_object',
-    #                                                             Params={'Bucket': ibuck,
-    #                                                                     'Key': ikey},
-    #                                                             ExpiresIn=None)
-    # tasks.append(task)
+    descriptor = parseJSON(tmpdir, descriptor_obj, buck, cli, descriptor=True)[0]
+    invocation = parseJSON(tmpdir, invocation_obj, buck, cli, invocation=True)
+    summary = parseJSON(tmpdir, summary_objs, buck, cli, summary=True)
+    tasks = parseJSON(tmpdir, task_objs, buck, cli, task=True,
+                      data={"descriptor": descriptor,
+                            "invocation": invocation,
+                            "summary": summary})
 
     app.config["data"] = {"bucket": bucket,
                           "offset": offset,
                           "tasks" : tasks}
+
+
+def main():
+    clowdrloc = sys.argv[1]
+    app.config["clowdrloc"] = clowdrloc
+    updateRecord()
     app.run(host='0.0.0.0', debug=True)
 
 
