ee91ba270523b8dffdd77971d9f722e576e73d6f
Greg Kiar <gkiar07@gmail.com>
2018-07-05 17:12:24 -0400

    merged parsers for local and cloud; TODO: merge functions, other parsers, fix old var names

--- clowdr/driver.py
+++ clowdr/driver.py
@@ -7,7 +7,8 @@
 # Created by Greg Kiar on 2018-02-28.
 # Email: gkiar@mcin.ca
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
+import argparse
 import os.path as op
 import tempfile
 import json
@@ -140,7 +141,7 @@ def cluster(tool, invocation, clowdrloc, dataloc, cluster, **kwargs):
             slurm_args[k] = v
     job = Slurm(jobname, slurm_args)
 
-    script = "clowdr run {} -c {} --local"
+    script = "clowdr task {} -c {} --local"
     if kwargs.get("workdir"):
         script += " -w {}".format(kwargs["workdir"])
     if kwargs.get("volumes"):
@@ -235,69 +236,158 @@ def share(clowdrloc, **kwargs):
 def main(args=None):
     """main
     Command-line API wrapper for Clowdr as a CLI, not Python API.
+    For information about the command-line wrapper and arguments it accepts,
+    please try running "clowdr --help".
 
     Parameters
     ----------
     args: list
-        List of all command-line arguments being passed
+        List of all command-line arguments being passed.
 
     Returns
     -------
     int
-        The exit-code returned by the task being executed
+        The exit-code returned by the driver.
     """
-    desc = "Interface for launching Boutiques task locally and in the cloud"
-    parser = ArgumentParser("Clowdr CLI", description=desc)
-    subparsers = parser.add_subparsers(help="Modes of operation", dest="mode")
 
-    # Local Parser
-    parser_loc = subparsers.add_parser("local")
-    parser_loc.add_argument("tool", help="boutiques descriptor for a tool")
-    parser_loc.add_argument("invocation", help="input(s) for the tool")
-    parser_loc.add_argument("clowdrloc", help="location locally for clowdr")
-    parser_loc.add_argument("dataloc", help="location locally or s3 for data")
-    parser_loc.add_argument("--workdir", "-w", action="store")
-    parser_loc.add_argument("--volumes", "-v", action="append")
-    parser_loc.add_argument("--user", "-u", action="store_true")
+    # Create an outer argparser which can wrap subparsers for each function.
+    desc = """
+Scalable deployment and provenance-rich wrapper for Boutiques tools locally,
+on clusters, and in the cloud. For more information, go to our website:
 
-    parser_loc.add_argument("--verbose", "-V", action="store_true")
-    parser_loc.add_argument("--bids", "-b", action="store_true")
-    parser_loc.add_argument("--dev", "-d", action="store_true")
+     https://github.com/clowdr/clowdr.
+"""
+    parser = ArgumentParser("clowdr", description=desc,
+                            formatter_class=RawTextHelpFormatter)
+
+    htext = """Clowdr has several distinct modes of operation:
+  - local:  This mode allows you to develop your Clowdr execution, deploy
+            analyses on your local system, and deploy them on clusters.
+  - cloud:  This mode allows you to deploy your Clowdr exectuion on a cloud
+            resource. Currently, this only supports Amazon Web Services.
+  - share:  This mode launches a lightweight webserver for you to explore your
+            executions, monitor job progress, and share your results.
+  - task:   This mode is generally only for super-users. It is used by Clowdr
+            to launch your tasks and record provenance information from them
+            without you needing to call this option yourself. It can be useful
+            when debugging or re-running failed executions.
+"""
+    subparsers = parser.add_subparsers(dest="mode",
+                                       help=htext)
+
+    # Create the subparser for local/cluster execution.
+    desc = ("Manages local and cluster deployment. Ideal for development, "
+            "testing, executing on local resources, or deployment on a "
+            "computing cluster environment.")
+    parser_loc = subparsers.add_parser("local", description=desc)
+    parser_loc.add_argument("descriptor", type=argparse.FileType('r'),
+                            help="Local path to Boutiques descriptor for the "
+                                 "tool you wish to run. To learn about "
+                                 "descriptors and Boutiques, go to: "
+                                 "https://boutiques.github.io.")
+    parser_loc.add_argument("invocation",
+                            help="Local path to Boutiques invocation (or "
+                                 "directory containing multiple invocations) "
+                                 "for the analysis you wish to run. To learn "
+                                 "about invocations and Boutiques, go to: "
+                                 "https://boutiques.github.io.")
+    parser_loc.add_argument("provdir",
+                            help="Local directory for Clowdr provenance records"
+                                 " and other captured metadata to be stored. "
+                                 "This directory needs to exist prior to "
+                                 "running Clowdr.")
+
+    parser_loc.add_argument("--verbose", "-V", action="store_true",
+                            help="Toggles verbose output statements.")
+    parser_loc.add_argument("--dev", "-d", action="store_true",
+                            help="Launches only the first created task. This "
+                                 "is intended for development purposes.")
+    parser_loc.add_argument("--workdir", "-w", action="store",
+                            help="Specifies the working directory to be used "
+                                 "by the tasks created.")
+    parser_loc.add_argument("--volumes", "-v", action="append",
+                            help="Specifies any volumes to be mounted to the "
+                                 "container. This is usually related to the "
+                                 "path of any data files as specified in your "
+                                 "invocation(s).")
+    parser_loc.add_argument("--cluster", "-c", choices=["slurm"],
+                            help="If you wish to submit your local tasks to a "
+                                 "scheduler, you must specify it here. "
+                                 "Currently this only supports SLURM clusters.")
+    parser_loc.add_argument("--clusterargs", "-a", action="store",
+                            help="This allows users to supply arguments to the "
+                                 "cluster, such as specifying RAM or requesting"
+                                 " a certain amount of time on CPU. These are "
+                                 "provided in the form of key:value pairs, and "
+                                 "separated by commas. For example: "
+                                 "--clusterargs time:4:00,mem:2048,account:ABC")
+    parser_loc.add_argument("--jobname", "-n", action="store",
+                            help="If running on a cluster, and you wish to "
+                                 "specify a unique identifier to appear in the"
+                                 "submitted tasks, you can specify it with "
+                                 "this flag.")
+    parser_loc.add_argument("--simg", "-s", action="store",
+                            help="If the Boutiques descriptor summarizes a "
+                                 "tool wrapped in Singularity, and the image "
+                                 "has already been downloaded, this option "
+                                 "allows you to specify that image file.")
+    parser_loc.add_argument("--user", "-u", action="store_true",
+                            help="If the Boutiques descriptor summarizes a "
+                                 "tool wrapped in Docker, toggles propagating "
+                                 "the current user within the container.")
+    parser_loc.add_argument("--s3", action="store",
+                            help="Amazon S3 bucket and path for remote data. "
+                                 "Accepted in the format: s3://{bucket}/{path}")
+    parser_loc.add_argument("--bids", "-b", action="store_true",
+                            help="Indicates that the tool being launched is a "
+                                 "BIDS app. BIDS is a data organization format"
+                                 " in neuroimaging. For more information about"
+                                 " this, go to https://bids.neuroimaging.io.")
 
     parser_loc.set_defaults(func=local)
+    # parser_cls.set_defaults(func=cluster)
 
-    # Cluster Parser
-    parser_cls = subparsers.add_parser("cluster")
-    parser_cls.add_argument("tool", help="boutiques descriptor for a tool")
-    parser_cls.add_argument("invocation", help="input(s) for the tool")
-    parser_cls.add_argument("clowdrloc", help="location locally for clowdr")
-    parser_cls.add_argument("dataloc", help="location locally or s3 for data")
-    parser_cls.add_argument("cluster", help="cluster type", choices=["slurm"])
-    parser_cls.add_argument("--jobname", "-n", action="store")
-    parser_cls.add_argument("--slurm_args", action="store")
-    parser_cls.add_argument("--workdir", "-w", action="store")
-    parser_cls.add_argument("--volumes", "-v", action="append")
-    parser_cls.add_argument("--simg", "-s", action="store")
+    # Create the subparser for cloud execution.
+    desc = ("Manages local and cluster deployment. Ideal for development, "
+            "testing, executing on local resources, or deployment on a "
+            "computing cluster environment.")
+    parser_cld = subparsers.add_parser("cloud", description=desc)
+    parser_cld.add_argument("descriptor", type=argparse.FileType('r'),
+                            help="Local path to Boutiques descriptor for the "
+                                 "tool you wish to run. To learn about "
+                                 "descriptors and Boutiques, go to: "
+                                 "https://boutiques.github.io.")
+    parser_cld.add_argument("invocation",
+                            help="Local path to Boutiques invocation (or "
+                                 "directory containing multiple invocations) "
+                                 "for the analysis you wish to run. To learn "
+                                 "about invocations and Boutiques, go to: "
+                                 "https://boutiques.github.io.")
+    parser_cld.add_argument("provdir",
+                            help="Local directory for Clowdr provenance records"
+                                 " and other captured metadata to be stored. "
+                                 "This directory needs to exist prior to "
+                                 "running Clowdr.")
+    parser_cld.add_argument("s3",
+                            help="Amazon S3 bucket and path for remote data. "
+                                 "Accepted in the format: s3://{bucket}/{path}")
+    parser_cld.add_argument("cloud", choices=["aws"],
+                            help="cloud endpoint")
+    parser_cld.add_argument("auth",
+                            help="credentials for the remote resource")
 
-    parser_cls.add_argument("--verbose", "-V", action="store_true")
-    parser_cls.add_argument("--bids", "-b", action="store_true")
-    parser_cls.add_argument("--dev", "-d", action="store_true")
-
-    parser_cls.set_defaults(func=cluster)
-
-    # Cloud Parser
-    parser_cld = subparsers.add_parser("cloud")
-    parser_cld.add_argument("tool",  help="boutiques descriptor for a tool")
-    parser_cld.add_argument("invocation", help="input(s) for the tool")
-    parser_cld.add_argument("clowdrloc", help="location on s3 for clowdr")
-    parser_cld.add_argument("dataloc", help="location on s3 of data")
-    parser_cld.add_argument("cloud", help="cloud endpoint", choices=["aws"])
-    parser_cld.add_argument("auth", help="credentials for the remote resource")
-    parser_cld.add_argument("--region", "-r", action="store")
-
-    parser_cld.add_argument("--verbose", "-V", action="store_true")
-    parser_cld.add_argument("--bids", "-b", action="store_true")
-    parser_cld.add_argument("--dev", "-d", action="store_true")
+    parser_cld.add_argument("--verbose", "-V", action="store_true",
+                            help="Toggles verbose output statements.")
+    parser_cld.add_argument("--dev", "-d", action="store_true",
+                            help="Launches only the first created task. This "
+                                 "is intended for development purposes.")
+    parser_cld.add_argument("--region", "-r", action="store",
+                            help="")
+    parser_cld.add_argument("--bids", "-b", action="store_true",
+                            help="Indicates that the tool being launched is a "
+                                 "BIDS app. BIDS is a data organization format"
+                                 " in neuroimaging. For more information about"
+                                 " this, go to https://bids.neuroimaging.io.")
 
     parser_cld.set_defaults(func=cloud)
 
@@ -308,18 +398,18 @@ def main(args=None):
 
     parser_shr.set_defaults(func=share)
 
-    # Run Parser
-    parser_run = subparsers.add_parser("run")
-    parser_run.add_argument("metadata", help="task metadata file")
-    parser_run.add_argument("--clowdrloc", "-c", action="store",
-                            help="task output directory")
-    parser_run.add_argument("--local", "-l", action="store_true")
-    parser_run.add_argument("--workdir", "-w", action="store")
-    parser_run.add_argument("--volumes", "-v", action="append")
+    # Task Parser
+    parser_task = subparsers.add_parser("task")
+    parser_task.add_argument("metadata", help="task metadata file")
+    parser_task.add_argument("--clowdrloc", "-c", action="store",
+                             help="task output directory")
+    parser_task.add_argument("--local", "-l", action="store_true")
+    parser_task.add_argument("--workdir", "-w", action="store")
+    parser_task.add_argument("--volumes", "-v", action="append")
 
-    parser_run.add_argument("--verbose", "-V", action="store_true")
+    parser_task.add_argument("--verbose", "-V", action="store_true")
 
-    parser_run.set_defaults(func=run)
+    parser_task.set_defaults(func=run)
 
     # Parse arguments
     inps = parser.parse_args(args) if args is not None else parser.parse_args()
