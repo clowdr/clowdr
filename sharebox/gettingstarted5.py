4111add0ea0eb900bee56d1355fc80897edcd2bf
gkiar <gkiar07@gmail.com>
2018-08-17 18:48:43 -0400

    updated filtering

--- sharebox/gettingstarted5.py
+++ sharebox/gettingstarted5.py
@@ -13,26 +13,36 @@ app = dash.Dash()
 app.scripts.config.serve_locally = True
 # app.css.config.serve_locally = True
 
-DF_WALMART = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/1962_2006_walmart_store_openings.csv')
-
 DF_GAPMINDER = pd.read_csv(
     'https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv'
 )
 DF_GAPMINDER = DF_GAPMINDER[DF_GAPMINDER['year'] == 2007]
 DF_GAPMINDER.loc[0:20]
 
-DF_USAGE = pd.read_csv('./sharebox/clowdr-summary.csv')
+with open('./sharebox/clowdr-summary.json') as usage_fhandle:
+    experiment_dict = json.load(usage_fhandle)
 
+table_dict = []
+for exp in experiment_dict:
+    table_dict += [{'Task': exp['Task ID']}]
+table_dict 
+#plotting_dict() = 1
+#param_dict() = 1
+
+print(experiment_dict[0].keys())
+
+#import pdb;pdb.set_trace()
+#table_columns = ['Task ID', 'Tool Name', 'Time: Total (s)']
 
 app.layout = html.Div([
     html.H4('Gapminder DataTable'),
     dt.DataTable(
         # rows=DF_GAPMINDER.to_dict('records'),
-        rows=DF_USAGE.to_dict('records'),
+        rows=table_dict,
 
         # optional - sets the order of columns
         # columns=sorted(DF_GAPMINDER.columns),
-        columns=DF_USAGE.columns,
+        columns=list(table_dict[0].keys()),
 
         row_selectable=True,
         filterable=True,
@@ -49,15 +59,17 @@ app.layout = html.Div([
 
 @app.callback(
     Output('datatable-gapminder', 'selected_row_indices'),
-    [Input('graph-gapminder', 'clickData')],
+    [Input('graph-gapminder', 'clickData'),
+     Input('datatable-gapminder', 'rows')],
     [State('datatable-gapminder', 'selected_row_indices')])
-def update_selected_row_indices(clickData, selected_row_indices):
+def update_selected_row_indices(clickData, rows, selected_row_indices):
     if clickData:
         for point in clickData['points']:
-            if point['pointNumber'] in selected_row_indices:
-                selected_row_indices.remove(point['pointNumber'])
+            curve = point['curveNumber'] // 3
+            if curve in selected_row_indices:
+                selected_row_indices.remove(curve)
             else:
-                selected_row_indices.append(point['pointNumber'])
+                selected_row_indices.append(curve)
     return selected_row_indices
 
 
@@ -66,32 +78,45 @@ def update_selected_row_indices(clickData, selected_row_indices):
     [Input('datatable-gapminder', 'rows'),
      Input('datatable-gapminder', 'selected_row_indices')])
 def update_figure(rows, selected_row_indices):
-    dff = pd.DataFrame(rows)
+    rows = [exp
+            for row in rows
+            for exp in experiment_dict
+            if row['Task'] == exp['Task ID']]
     fig = plotly.tools.make_subplots(
         rows=3, cols=1,
-        subplot_titles=('Life Expectancy', 'GDP Per Capita', 'Population',),
+        subplot_titles=('Memory Profile', 'Time Profile', 'Memory Profile',),
         shared_xaxes=True)
-    marker = {'color': ['#0074D9']*len(dff)}
+    marker = {'color': ['#0074D9']*len(rows)}
     for i in (selected_row_indices or []):
         marker['color'][i] = '#FF851B'
-    fig.append_trace({
-        'x': dff['country'],
-        'y': dff['lifeExp'],
-        'type': 'bar',
-        'marker': marker
-    }, 1, 1)
-    fig.append_trace({
-        'x': dff['country'],
-        'y': dff['gdpPercap'],
-        'type': 'bar',
-        'marker': marker
-    }, 2, 1)
-    fig.append_trace({
-        'x': dff['country'],
-        'y': dff['pop'],
-        'type': 'bar',
-        'marker': marker
-    }, 3, 1)
+    for i, row in enumerate(rows):
+        fig.append_trace({
+            'x': row['Time: Series (s)'],
+            'y': row['RAM: Series (MB)'],
+            'mode': 'lines+markers',
+            'line': {'color': marker['color'][i]},
+            'customdata': (row, i),
+            'opacity': 0.6,
+            'name': 'Task {}'.format(row['Task ID'])
+        }, 1, 1)
+        fig.append_trace({
+            'x': row['Time: Series (s)'],
+            'y': row['Time: Series (s)'],
+            'mode': 'lines+markers',
+            'line': {'color': marker['color'][i]},
+            'customdata': (row, i),
+            'opacity': 0.8,
+            'name': 'Task {}'.format(row['Task ID'])
+        }, 2, 1)
+        fig.append_trace({
+            'x': row['Time: Series (s)'],
+            'y': row['RAM: Series (MB)'],
+            'mode': 'lines',
+            'line': {'color': marker['color'][i]},
+            'customdata': (row, i),
+            'opacity': 0.8,
+            'name': 'Task {}'.format(row['Task ID'])
+        }, 3, 1)
     fig['layout']['showlegend'] = False
     fig['layout']['height'] = 800
     fig['layout']['margin'] = {
